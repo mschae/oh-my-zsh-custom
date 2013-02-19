86921423f37758c0d96ad8460a9fe24a4bd26ced
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2013-02-19 11:59:27 +0100

    Update start/stop commands

--- plugins/mschae/mschae.plugin.zsh
+++ plugins/mschae/mschae.plugin.zsh
@@ -19,28 +19,15 @@ alias fs='bundle install && foreman start'
 alias grd='schemaload && testmigrate && guard'
 
 # Start/Stop Commands
-alias startpg='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias stoppg='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+alias 'pg.start'='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias 'pg.stop'='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
-alias startredis='redis-server /usr/local/etc/redis.conf'
-alias stopredis='kill `cat /usr/local/var/run/redis.pid`'
+alias 'redis.start'='redis-server /usr/local/etc/redis.conf'
+alias 'redis.stop'='kill `cat /usr/local/var/run/redis.pid`'
+
+alias 'mysql.start'='mysql.server start'
+alias 'mysql.stop'='mysql.server stop'
 
 unsetopt correct_all
 
 ZSH_THEME="bira"
-
-
-# Set to this to use case-sensitive completion
-# CASE_SENSITIVE="true"
-
-# Comment this out to disable weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment following line if you want to disable colors in ls
-# DISABLE_LS_COLORS="true"
-
-# Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
